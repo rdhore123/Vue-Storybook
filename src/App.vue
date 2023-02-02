ba428b711a6df7dcda3648bfc83feb5be9df5d85
Ravindra Dhore <r.dhore@easternenterprise.com>
2023-02-02 14:55:06 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>dsd <Button size="large" :primary="false" label="Submit"></Button></div>
+</template>
+<script>
+import Button from "./stories/Button.vue";
+
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
